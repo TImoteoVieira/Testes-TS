04bfc638e340a18a17c71a63d26bebd66cd5f359
TImoteoVieira <timoteo7charles777@gmail.com>
2022-01-15 15:21:33 -0300

    [change] - change on explication

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,15 @@ import express from "express";
 
 const app = express();
 
+app.get("/route1", (req, res) => {
+    return res.send("Testando Node + Type Script")
+})
+
+app.listen(3000, () => {
+    console.log('Server up')
+});
+
+
 /*
  ====== Verbos http =====
 get    -> buscar dado
@@ -11,10 +20,11 @@ delete -> deletar dado
 patch  -> alterar um dado específico
 */
 
-app.get("/route1", (req, res) => {
-    return res.send("Testando Node + Type Script")
-})
-
-app.listen(3000, () => {
-    console.log('Server up')
-});
+/*
+===== Tipos de parâmetros =======
+Routes Params -> http://xxx1411441342134
+Query Params  -> http://xxx?name=teclado
+Body Params   -> {
+                    "name":"teclado"
+                 } 
+*/
